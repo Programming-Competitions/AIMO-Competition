5341020a5a89c888b3d3ce0040a79a3d425759e9
Unlegendry <69323039+UnlegendayGamer@users.noreply.github.com>
2024-04-12 18:54:21 -0500

    Create arithmatic.py
    bubba

---
+++ arithmatic.py
@@ -0,0 +1,25 @@
+#Arithmatic
+def add(x, y):
+  total = x + y
+
+def subtract(x, y):
+  total = x - y
+
+def multiply(x, y):
+  total = x * y
+
+def divide(x, y):
+  total = x / y
+
+def exponent(x, y):
+  total = x ** y
+
+def percent(x, y):
+  total = (x / y) * 100
+
+def radical(x, y):
+  total = x ** (1/y)
+
+def exponent(x, y):
+  total = x ** y
+  
