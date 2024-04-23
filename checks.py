7ac76b7824c410ff305aafb3c2a39db41acbffb2
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-04-23 09:44:27 -0500

    Create checks.py

---
+++ checks.py
@@ -0,0 +1,12 @@
+# checklist in progress build, the "topics to look for"
+
+topics = {
+  "algebra":["linear equations","quadratic equations","polynomial functions","exponential functions","logarithmic functions","complex numbers","vectors","advanced polynomial theory"],
+  "geometry":["plane geometry","angles","area","volume","conic sections","coordinate geometry","Ptolemy’s theorem","Ceva’s theorem"],
+  "trignometry":["sine rule","cosine rule","unit circle","sin","cos","tan","cot","sec","cosec"],
+  "combinatorics":["permutations","combinations","Combinatorial extremum","graph theory"],
+  "numTheory":["prime number theory","division algorithims","modulo operations","rising power theorem"],
+  "arithSequencesSeries":[],
+  "probTheory":[],
+  "advancedGeomTheory":["median theorem","pythagorean theorem"],
+}
